e5307106dd9c69352ec56f7874eb23448a8a830e
adeaver <deaver.andrew@gmail.com>
2015-10-13 18:20:52 -0300

    Begin work on new site

---
+++ js/navbar.js
@@ -0,0 +1,108 @@
+var primary = "#3498DB"
+var secondary = "#ECF0F1"
+
+$(document).ready(function() {
+	// Assign navbar hovers
+	$("#nav_home").hover(function() {
+		$("#nav_home").css({
+			"background-color":primary,
+			"color":secondary
+		});
+		$("#nav_home").text("Click to Learn More")
+		$("#desc_nav_home").slideDown("slow");
+	}, function() {
+		$("#nav_home").css({
+			"background-color":secondary,
+			"color":primary
+		});
+		$("#desc_nav_home").slideUp("slow", function() {
+			$("#nav_home").text("Home");
+		});
+	});
+
+	$("#nav_projects").hover(function() {
+		$("#nav_projects").css({
+			"background-color":primary,
+			"color":secondary
+		});
+		$("#desc_nav_projects").slideDown("slow");
+		$("#nav_projects").text("Click to Learn More");
+	}, function() {
+		$("#nav_projects").css({
+			"background-color":secondary,
+			"color":primary
+		});
+		$("#desc_nav_projects").slideUp("slow", function() {
+			$("#nav_projects").text("Projects");
+		});
+	});
+
+	$("#nav_experience").hover(function() {
+		$("#nav_experience").css({
+			"background-color":primary,
+			"color":secondary
+		});
+		$("#desc_nav_experience").slideDown("slow");
+		$("#nav_experience").text("Click to Learn More");
+	}, function() {
+		$("#nav_experience").css({
+			"background-color":secondary,
+			"color":primary
+		});
+		$("#desc_nav_experience").slideUp("slow", function() {
+			$("#nav_experience").text("Experience");
+		});
+	});
+
+	$("#nav_passions").hover(function() {
+		$("#nav_passions").css({
+			"background-color":primary,
+			"color":secondary
+		});
+		$("#desc_nav_passions").slideDown("slow");
+		$("#nav_passions").text("Click to Learn More");
+	}, function() {
+		$("#nav_passions").css({
+			"background-color":secondary,
+			"color":primary
+		});
+		$("#desc_nav_passions").slideUp("slow", function() {
+			$("#nav_passions").text("Outside of Engineering");
+		});
+	});
+
+	$("#nav_contact").hover(function() {
+		$("#nav_contact").css({
+			"background-color":primary,
+			"color":secondary
+		});
+		$("#desc_nav_contact").slideDown("slow");
+		$("#nav_contact").text("Click to Learn More");
+	}, function() {
+		$("#nav_contact").css({
+			"background-color":secondary,
+			"color":primary
+		});
+		$("#desc_nav_contact").slideUp("slow", function() {
+			$("#nav_contact").text("Contact Me");
+		});
+	});
+
+	// Assign navbar links
+
+	$("#nav_home").click(function() {
+		$("#content").fadeOut("fast", function() {
+			$("#content").load("./srcs/header.html", function() {
+				$("#content").fadeIn("fast");
+			})
+		})
+	});
+
+	$("#nav_projects").click(function() {
+		$("#content").fadeOut("fast", function() {
+			$("#content").load("./srcs/projects.html", function() {
+				$("#content").fadeIn("fast");
+			})
+		})
+	});
+});
