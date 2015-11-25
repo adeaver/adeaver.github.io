689882921b2d3ecfc73f7d0de63a03e46072ddfc
adeaver <deaver.andrew@gmail.com>
2015-11-25 14:55:10 -0200

    Fixed weird toggle animation

--- js/mobile_nav.js
+++ js/mobile_nav.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 	$("#navbar_prompt").click(function() {
 		$("#navbar_content").slideToggle("slow", function() {
 			if($("#navbar_content").css("display") != "none") {
-				$("#main_title").text("Click to hide navigation bar");
+				$("#main_title").text("Hide navigation bar");
 			} else {
 				$("#main_title").text("Navigate");
 			}
