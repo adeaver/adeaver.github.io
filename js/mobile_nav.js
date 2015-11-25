f29fa15613abcb0d8116d47902d4b085837adafb
adeaver <deaver.andrew@gmail.com>
2015-11-25 16:05:09 -0200

    Restyled navigation bar

--- js/mobile_nav.js
+++ js/mobile_nav.js
@@ -1,11 +1,15 @@
 $(document).ready(function() {
 	$("#navbar_prompt").click(function() {
-		$("#navbar_content").slideToggle("slow", function() {
-			if($("#navbar_content").css("display") != "none") {
-				$("#main_title").text("Hide navigation bar");
-			} else {
+		if($("#navbar_content").css("top") != "0px") {
+			$("#navbar_content").animate({"top":"0px"}, function() {
+				$("#main_title").text("Hide Navigation Bar");
+				$("#navbar_content").css("overflow-y", "scroll");
+			});
+		} else {
+			$("#navbar_content").animate({"top":"90%"}, function() {
 				$("#main_title").text("Navigate");
-			}
-		});
-	})
+				$("#navbar_content").css("overflow-y", "hidden");
+			});
+		}
+	});
 });
